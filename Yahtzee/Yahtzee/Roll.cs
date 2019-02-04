6e9b23dd033a9395e79d6321813794a54e823cd4
Talal <talfahad@umich.edu>
2019-02-03 21:26:52 -0500

    Update

--- Yahtzee/Yahtzee/Roll.cs
+++ Yahtzee/Yahtzee/Roll.cs
@@ -8,45 +8,38 @@ namespace Yahtzee
 {
     public class Roll
     {
-           
+        IRandom randomVal;
         YahtzeeForm gameInstance;
 
         public int[] HoldDice = { 0, 0, 0, 0, 0 };
         public int[] DiceName = { 0, 0, 0, 0, 0 };
-        public Roll ()
+        
+        public Roll()
         {
-
-        }
-        public int RollDie()
-        {
-            Random random = new Random();
-            int randomHold = new int();
-            randomHold = random.Next(1, 7);
-            return randomHold;
+            this.randomVal = randomVal;       
         }
 
         public void RollResults()
         {
-            
             if (HoldDice[0] == 0)
             {
-                DiceName[0] = RollDie();
+                DiceName[0] = randomVal.Next(1,7);
             }
             if (HoldDice[1] == 0)
             {
-                DiceName[1] = RollDie();
+                DiceName[1] = randomVal.Next(1, 7);
             }
             if (HoldDice[2] == 0)
             {
-                DiceName[2] = RollDie();
+                DiceName[2] = randomVal.Next(1, 7);
             }
             if (HoldDice[3] == 0)
             {
-                DiceName[3] = RollDie();
+                DiceName[3] = randomVal.Next(1, 7);
             }
             if (HoldDice[4] == 0)
             {
-                DiceName[4] = RollDie();
+                DiceName[4] = randomVal.Next(1, 7);
             }
         }   
     }
